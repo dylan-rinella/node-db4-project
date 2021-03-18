1bc1004a91dd17fc6e480ebd9b9356ea90917408
dylan-rinella <rinelladylan@aim.com>
2021-03-18 16:51:26 -0400

    database created

---
+++ api/recipes/recipes-model.js
@@ -0,0 +1,11 @@
+const db = require('../../data/db-config')
+
+const get = () => {
+  return db('recipes')
+}
+
+
+
+module.exports = {
+
+}
---
+++ data/migrations/20210318203844_make-recipes-table.js
@@ -0,0 +1,41 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('recipes', table => {
+      table.increments('recipe_id')
+      table.string('recipe_name', 128)
+      table.integer('step_id')
+        .unsigned()
+        .notNullable()
+        .references('step_id')
+        .inTable('steps')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+
+    .createTable('steps', table => {
+      table.increments('step_id')
+      table.integer('step_number').notNullable()
+      table.string('step_instructions', 128)
+      table.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
+
+    .createTable('ingredients', table => {
+      table.increments('ingredient_id')
+      table.string('ingredient_name', 128)
+      table.float('quantity')
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('ingredients')
+    .dropTableIfExists('steps')
+    .dropTableIfExists('recipes')
+};
